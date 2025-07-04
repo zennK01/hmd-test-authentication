ec6d08b2fa3f08e71ccf87bedd0d0384fb7c3299
tin.nd <tinn948@gmail.com>
2025-07-04 22:53:01 +0700

    feat: setup connect mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: '127.0.0.1',
+      port: 8811,
+      username: 'root',
+      password: 'zenn',
+      database: 'test',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
